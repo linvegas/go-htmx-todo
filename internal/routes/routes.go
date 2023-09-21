58eeed6dab84fd2c6c991faa253ca8a0447f3ae3
MisterConscio <saulo.costam@gmail.com>
2023-09-21 20:53:04 -0300

    Delete todo hability

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -12,28 +12,28 @@ type Todo struct {
   Done        bool
 }
 
+var todolist = map[string][]Todo {
+  "Todos": {
+    {Description: "Learn Go", Done: false},
+    {Description: "Finish Bioshock", Done: false},
+    {Description: "Build shape", Done: true},
+  },
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  tmpl := template.Must(template.ParseFiles(
-    "web/pages/index.html",
-    ))
-
-  todolist := map[string][]Todo {
-    "Todos": {
-      {Description: "Learn Go", Done: false},
-      {Description: "Finish Bioshock", Done: false},
-      {Description: "Build shape", Done: true},
-    },
-  }
-
+  tmpl := template.Must(template.ParseFiles("web/pages/index.html"))
   tmpl.Execute(w, todolist)
 }
 
 func addTodoHandler(w http.ResponseWriter, r *http.Request) {
   description := r.FormValue("todo-desc")
+  newTodo := Todo{Description: description, Done: false}
+
   log.Print("HTMX request received")
   log.Printf("Form 'todo-desc': %s\n", description)
+
   tmpl := template.Must(template.ParseFiles("web/pages/index.html"))
-  tmpl.ExecuteTemplate(w, "todo-list-item", Todo{Description: description, Done: false})
+  tmpl.ExecuteTemplate(w, "todo-list-item", newTodo)
 }
 
 func NewRouter() http.Handler {
@@ -41,7 +41,6 @@ func NewRouter() http.Handler {
 
   mux.HandleFunc("/", indexHandler)
   mux.HandleFunc("/add-todo", addTodoHandler)
-  // mux.HandleFunc("/api/data", apiDataHandler)
 
   return mux
 }
